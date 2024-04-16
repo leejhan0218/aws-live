97633fdff031fe61f98f861307f9bd3815351e71
leejhan0218 <167171951+leejhan0218@users.noreply.github.com>
2024-04-16 14:48:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c54qgucu6s90.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "leejinghan-employee"
+customregion = "us-east-1"
+
